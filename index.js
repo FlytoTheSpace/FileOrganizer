ae10bdf3b643cf88192f372d78accfeb78e5822d
FlytoTheSpace <flytothespace.64@gmail.com>
2024-01-09 20:59:38 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const fs = require('fs');
 const path = require('path');
+
 // Made by
 // https://www.youtube.com/@flytothespace
+// https://github.com/flytothespace/
+// Discord: flytothespace.
 
 const files = fs.readdirSync(path.join(__dirname, 'files'));
 files.forEach(fileName => {
     const Extention = path.extname(fileName).replace('.', '')
     try {
         fs.mkdirSync(path.join(__dirname, `files/${Extention}`))
+        console.log(`Created new Directory ${Extention}`)
     } catch (error) {
         if (error.message.includes('file already exists')){}
         else {
             console.log(error.message)
         }
     }
+    
+    console.log(`Moving ${fileName} to Directory /files/${ Extention }/${ fileName }`)
     fs.renameSync(path.join(__dirname, `files/${fileName}`), path.join(__dirname, `files/${Extention}/${fileName}`))
 });
-console.log(files)
+Console.log("Done! enjoy")
